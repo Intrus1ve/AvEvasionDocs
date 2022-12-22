2a993acb4d19de0bf6045b1c602d668209d22d45
Intrus1ve <98790607+Intrus1ve@users.noreply.github.com>
2022-12-23 00:58:14 +0300

    Add files via upload

---
+++ Unkillable Process.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Unkillable_Process
+{
+    public partial class Form1 : Form
+    {      
+        
+    
+
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern int NtSetInformationProcess(IntPtr hProcess, int processInformationClass, ref int processInformation, int processInformationLength);
+
+
+
+        public void critical(int status)
+        {
+            int BreakOnTermi = 0x1D;
+            Process.EnterDebugMode();
+            NtSetInformationProcess(Process.GetCurrentProcess().Handle, BreakOnTermi,
+            ref status, sizeof(int));
+
+        }
+
+
+         
+
+       
+    }
+    }
+
+
